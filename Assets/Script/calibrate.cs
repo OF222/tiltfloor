5b8836447fc0e3cb53b185e21c68b74a9eceb090
OF222 <200441139@ccmailg.meijo-u.ac.jp>
2024-01-16 16:43:14 +0900

    add floor calibration

---
+++ Assets/Script/calibrate.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class calibrate : MonoBehaviour
+{
+    private float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.up * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+    }
+}
